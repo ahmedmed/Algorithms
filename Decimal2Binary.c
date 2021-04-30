1eeee14b6fc59b257332beea0766e23c37a799bd
ahmedmed <31887818+ahmedmed@users.noreply.github.com>
2021-04-30 15:01:08 +0200

    initial commit

---
+++ Decimal2Binary.c
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define ull unsigned long long
+#define lld long long
+#define mod 1000000007
+#define ld long
+#define ldo long double
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    lld decimal;
+    string binary;
+    cin>>decimal;
+
+    while(decimal/2 != 0){
+        binary += (char)((decimal%2)+48);
+        decimal /= 2;
+    }
+
+    binary += (char)(decimal + 48);
+    reverse(begin(binary), end(binary));
+    cout<<binary;
+
+    return 0;
+}
